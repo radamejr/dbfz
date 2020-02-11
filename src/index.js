c2ad44d9e1e4e7ef2505628440faca45c65a2eea
radamejr <50850876+radamejr@users.noreply.github.com>
2020-02-10 17:15:50 -0700

    added ability for pages to load in IE

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import 'react-app-polyfill/ie11';
+import 'react-app-polyfill/stable';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "../node_modules/jquery/dist/jquery.min.js";
 import "../node_modules/bootstrap/dist/js/bootstrap.min.js";
