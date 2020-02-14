c83fdd19ac292b7e3efb1fcb37f4b8c1e8567b72
radamejr <50850876+radamejr@users.noreply.github.com>
2020-02-13 18:48:07 -0700

    added feedbacklink

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -14,7 +14,11 @@ class Footer extends Component {
                 <footer className="footer">
                     <div className="col footer-container">
                         <div className="row pl-3">
-
+                            
+                            <div className="col">
+                                <br></br>
+                                <a href="https://forms.gle/rKNVi7CGnZwG6MB36" target="_blank" rel="noopener noreferrer" role="button">Feedback</a>
+                            </div>
                         </div>
                     </div>
                 </footer>
