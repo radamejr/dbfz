f03bed3595a88d65d5d2032b8952ecb391822cfa
radamejr <50850876+radamejr@users.noreply.github.com>
2020-02-16 08:09:51 -0700

    updated home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -11,7 +11,16 @@ class Home extends Component {
                     Welcome to Frame-ha-me-ha! This was a personal project that I have been working on to try and bring together multpile assets and resources. Recently <a href="https://play.google.com/store/apps/details?id=com.Jonathan.Thats_Plus" target="_blank" rel="noopener noreferrer" role="button">That's Plus</a>(If you havent checked it out, you should.), was released by <a href="https://twitter.com/jonathan_tene" target="_blank" rel="noopener noreferrer" role="button">Jonathan Tene</a> which was a huge motivator to finish this project. 
                     <br></br>
                     <br></br>
-                    Hopefully this can help fill a gap for any non android users out there. Or if you are looking to get this information on a PC/Mac without digging into the excel sheets. Speaking of those sheets this entire thing is not possible without <a href="https://twitter.com/RinSenpaiii" target="_blank" rel="noopener noreferrer" role="button">@Rin-senpaii</a> and <a href ="https://twitter.com/rafestation" target="_blank" rel="noopener noreferrer" role="button">@Turtleon</a> putting in an amazing amount of work on the doc's that enabled this kind of resource. I would also like to thank whoever is managing the hitbox viewer but I cannot seem to find their information, if you have it let me know. 
+                    Hopefully this can help fill a gap for any non android users out there. Or if you are looking to get this information on a PC/Mac without digging into the excel sheets. Speaking of those sheets this entire thing is not possible without <a href="https://twitter.com/RinSenpaiii" target="_blank" rel="noopener noreferrer" role="button">@Rin-senpaii</a> and <a href ="https://twitter.com/rafestation" target="_blank" rel="noopener noreferrer" role="button">@Turtleon</a> putting in an amazing amount of work on the doc's that enabled this kind of resource. Also thank you to <a href ="https://twitter.com/CoderDBFZ" target="_blank" rel="noopener noreferrer" role="button">@CoderDBFZ</a> for the hitbox viewer.
+                    <a href="https://docs.google.com/spreadsheets/d/1-p29UmRGIPF6n17ddOEtYfLcn_KRlE2VH6tE61P5UM8/edit#gid=768448487" target="_blank" rel="noopener noreferrer" role="button">Frame Data</a>
+                    <a href="https://docs.google.com/spreadsheets/d/1IIpmO2wDU2KZV7tYQhcK3hB0Z_Mg1H18HFeDox9LZ9o/edit#gid=0" target="_blank" rel="noopener noreferrer" role="button">@Rin-senpaii</a>
+                    <a href="https://docs.google.com/spreadsheets/d/17K1onCbItyLc8njouC_9-j7PZBT9rMqpn_Je55BINl4/edit#gid=0" target="_blank" rel="noopener noreferrer" role="button">Super Damage</a>
+                    <a href="https://docs.google.com/spreadsheets/d/17K1onCbItyLc8njouC_9-j7PZBT9rMqpn_Je55BINl4/edit#gid=0" target="_blank" rel="noopener noreferrer" role="button">5L / 2L Data</a>
+
+                    <br></br>
+                    <br></br>
+                    This resource is not maintained by the folks mentioned above, rather I am updating and reflecting in a more digestable manner what they have created. None of the individuals are involved with this resource. If you would like to see their original data in all of its excel glory. You can view them:
+
                     <br></br>
                     <br></br>
                     Thank you everyone who has contributed to the DBFZ community to make this resource a possibility, special shout-out to <a href="https://twitter.com/rathfgc" target="_blank" rel="noopener noreferrer" role="button">RathFGC</a> (Gotta rep our AZ fam) and ALL DBFZ content creators, for creating awesome videos that can help new and old comers alike. And remember to tech those dragon rushes.
@@ -25,3 +34,4 @@ class Home extends Component {
 }
  
 export default Home;
+
